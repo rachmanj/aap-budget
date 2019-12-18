caa06454ac296a31aff28ea711b7e2fde25213e8
omanof <rachmanj@gmail.com>
2019-12-18 22:17:09 +0800

    initial commit

--- application/views/dashboard_v.php
+++ application/views/dashboard_v.php
@@ -66,7 +66,7 @@
                   <li><!-- start message -->
                     <a href="#">
                       <div class="pull-left">
-                        <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="<?= base_url(); ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Support Team
